c1cba30c88183ddd75c150ddc14dfa9ed881e7e4
Angel Morales <a.jesse3001@gmail.com>
2018-03-29 09:58:15 -0600

    Initial Commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Morales, Angel (MX - Mexico) on 29/03/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

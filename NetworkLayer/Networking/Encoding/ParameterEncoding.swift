5f69fb0db388a16f4001384e0b7b6cb01ade2552
Angel Morales <a.jesse3001@gmail.com>
2018-03-30 17:10:11 -0600

    Projecto finalizado

---
+++ NetworkLayer/Networking/Encoding/ParameterEncoding.swift
@@ -0,0 +1,9 @@
+//
+//  ParameterEncoding.swift
+//  NetworkLayer
+//
+//  Created by Morales, Angel (MX - Mexico) on 29/03/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import Foundation
---
+++ NetworkLayer/Networking/Service/HTTPTask.swift
@@ -0,0 +1,9 @@
+//
+//  HTTPTask.swift
+//  NetworkLayer
+//
+//  Created by Morales, Angel (MX - Mexico) on 29/03/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import Foundation
